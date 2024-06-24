06af49d6ec596a90d57322f47ea377a19ad3b49e
Molka Tayahi <tayahi.molka@gmail.com>
2024-06-24 11:25:36 +0200

    Update to drupal 10.3

--- drupal/web/index.php
+++ drupal/web/index.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 $autoloader = require_once 'autoload.php';
 
 $kernel = new DrupalKernel('prod', $autoloader);
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
